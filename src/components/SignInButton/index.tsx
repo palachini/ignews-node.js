b9db320d7ad334c2a703a0b662f85fc5933d722f
palachini <mateuspalachini@gmail.com>
2022-07-14 17:23:52 -0400

    create page home but not button subscriber

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,7 +1,7 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
-import style from './styles.module.scss';
+import styles from './styles.module.scss';
 
 export function SignInButton() {
   const isUserLoggedIn = true;
@@ -9,16 +9,16 @@ export function SignInButton() {
   return isUserLoggedIn ? (
     <button 
       type="button"
-      className={style.signInButton}
+      className={styles.signInButton}
     >
       <FaGithub color="#04d361"/>
       Mateus Palachini
-      <FiX color="#737380" className={style.closeIcon} />
+      <FiX color="#737380" className={styles.closeIcon} />
     </button> 
   ) : (
     <button 
       type="button"
-      className={style.signInButton}
+      className={styles.signInButton}
     >
       <FaGithub color="#eba417"/>
       Sign in with Github
