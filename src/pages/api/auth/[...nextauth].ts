1d035c684cb792b105b0539796f2090a35050f13
palachini <mateuspalachini@gmail.com>
2022-07-20 03:29:37 -0400

    create new collection in users to login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from "faunadb";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
@@ -8,6 +11,29 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_SECRET,
         }),
     ],
+    // jwt: {
+    //     signingKey: process.env.SIGNING_KEY,
+    // },
+    callbacks: {
+        async signIn({ user, account, profile, email, credentials }) {
+            const emailUser = user.email
+
+            try {
+                await fauna.query(
+                    q.Create(
+                        q.Collection('users'),
+                        { data: { emailUser } }
+                    )
+                )
+
+                return true;
+            } catch {
+                return false;
+            }
+
+        }
+    },
+    secret: process.env.NEXTAUTH_SECRET
 })
 
 
